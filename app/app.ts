17a584ac2850e68d3d9896e252c4168091c627cc
Manoel de Souza Silva Lima <manoel.lima@tjac.jus.br>
2021-08-05 22:50:06 -0500

    Evitando a compilacao de arquivos com erro

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,6 @@
 import { Negociacao } from "./models/negociacao";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
+const negociacao = new Negociacao(new Date());
 console.log(negociacao);
 negociacao.quantidade = 10000;
 console.log(negociacao);
