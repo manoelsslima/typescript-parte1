0154c95d4b4495e14d0e8a038a3a8072fd7e0c7a
Manoel de Souza Silva Lima <manoel.lima@tjac.jus.br>
2021-08-07 00:45:16 -0500

    Outra forma de declarar construtor mantendo a imutabilidade dos atributos

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,7 +1,9 @@
 export class Negociacao {
 
+    constructor(public readonly data: Date, public readonly quantidade: number, public readonly valor: number) {}
+
     // outra forma de modelar a classe. O TS infere os atributos da classe
-    constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
+    //constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
     /*
     private _data: Date;
     private _quantidade: number;
@@ -12,7 +14,7 @@ export class Negociacao {
         this._quantidade = quantidade;
         this._valor = valor;
     }
-    */
+    
     get data(): Date {
         return this._data;
     }
@@ -24,8 +26,8 @@ export class Negociacao {
     get valor(): number {
         return this._valor;
     }
-
+    */
     get volume(): number {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,10 +1,11 @@
 export class Negociacao {
-    // outra forma de modelar a classe. O TS infere os atributos da classe
-    constructor(_data, _quantidade, _valor) {
-        this._data = _data;
-        this._quantidade = _quantidade;
-        this._valor = _valor;
+    constructor(data, quantidade, valor) {
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
+    // outra forma de modelar a classe. O TS infere os atributos da classe
+    //constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
     /*
     private _data: Date;
     private _quantidade: number;
@@ -15,17 +16,20 @@ export class Negociacao {
         this._quantidade = quantidade;
         this._valor = valor;
     }
-    */
-    get data() {
+    
+    get data(): Date {
         return this._data;
     }
-    get quantidade() {
+
+    get quantidade(): number {
         return this._quantidade;
     }
-    get valor() {
+
+    get valor(): number {
         return this._valor;
     }
+    */
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
