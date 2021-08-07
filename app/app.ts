8efa27390d711237b6157d99dd68a2635451ea3b
Manoel de Souza Silva Lima <manoel.lima@tjac.jus.br>
2021-08-07 00:11:48 -0500

    Invocação do controller

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,9 @@
-import { Negociacao } from "./models/negociacao";
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao);
-//negociacao.quantidade = 10000;
-console.log(negociacao);
+const controller = new NegociacaoController();
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault(); // evita o reload da página
+    controller.adiciona();
+});
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,7 @@
-import { Negociacao } from "./models/negociacao";
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao);
-//negociacao.quantidade = 10000;
-console.log(negociacao);
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault(); // evita o reload da página
+    controller.adiciona();
+});
